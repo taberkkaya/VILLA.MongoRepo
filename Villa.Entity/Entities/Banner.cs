6e350dcef76cb12745891a46b885d1331bd9ff05
taberkkaya <taberkkaya@taberkkaya.com>
2024-09-16 22:59:15 +0300

    entities were created

---
+++ Villa.Entity/Entities/Banner.cs
@@ -0,0 +1,9 @@
+﻿namespace Villa.Entity.Entities;
+
+public class Banner : BaseEntity
+{
+    public string? City { get; set; }
+    public string? Titler { get; set; }
+    public string? ImgUrl { get; set; }
+
+}
---
+++ Villa.Entity/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace Villa.Entity.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string? ImgUrl { get; set; }
+        public string? Category { get; set; }
+        public string? Price { get; set; }
+        public string? Title { get; set; }
+        public int BedroomCount { get; set; }
+        public int BathRoomCount { get; set; }
+        public int Area { get; set; }
+        public int Floor { get; set; }
+        public int ParkingCount { get; set; }
+
+    }
+}
