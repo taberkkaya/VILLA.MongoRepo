f52e48e8d1155a3cbf01d5b9e7619b2a2ede6f23
taberkkaya <taberkkaya@taberkkaya.com>
2024-09-17 13:04:17 +0300

    ServiceExtensions fixed

---
+++ Villa.WebUI/Extensions/ServiceExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Villa.Business.Abstract;
+using Villa.Business.Concrete;
+using Villa.DataAccess.Abstract;
+using Villa.DataAccess.EntityFramework;
+using Villa.DataAccess.Repositories;
+
+namespace Villa.WebUI.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void AddServiceExtensions(this IServiceCollection services)
+        {
+            #region Registration
+            services.AddScoped<IBannerDal, EfBannerDal>();
+            services.AddScoped<IBannerService, BannerManager>();
+
+            services.AddScoped<IContactDal, EfContactDal>();
+            services.AddScoped<IContactService, ContactManager>();
+
+            services.AddScoped<ICounterDal, EfCounterDal>();
+            services.AddScoped<ICounterService, CounterManager>();
+
+            services.AddScoped<IDealDal, EfDealDal>();
+            services.AddScoped<IDealService, DealManager>();
+
+            services.AddScoped<IFeatureDal, EfFeatureDal>();
+            services.AddScoped<IFeatureService, FeatureManager>();
+
+            services.AddScoped<IMessageDal, EfMessageDal>();
+            services.AddScoped<IMessageService, MessageManager>();
+
+            services.AddScoped<IProductDal, EfProductDal>();
+            services.AddScoped<IProductService, ProductManager>();
+
+            services.AddScoped<IQuestDal, EfQuestDal>();
+            services.AddScoped<IQuestService, QuestManager>();
+
+            services.AddScoped<IVideoDal, EfVideoDal>();
+            services.AddScoped<IVideoService, VideoManager>();
+
+            services.AddScoped(typeof(IGenericDal<>), typeof(GenericRepository<>));
+            services.AddScoped(typeof(IGenericService<>), typeof(GenericManager<>));
+            #endregion
+        }
+    }
+}
+
