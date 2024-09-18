4c9d5fd4b98d296712b6a556809293f31f375347
taberkkaya <taberkkaya@taberkkaya.com>
2024-09-18 17:35:31 +0300

    sidebar arrangement

--- Villa.Entity/Entities/Message.cs
+++ Villa.Entity/Entities/Message.cs
@@ -12,6 +12,7 @@ namespace Villa.Entity.Entities
         public string? Email { get; set; }
         public string? Subject { get; set; }
         public string? MessageContent { get; set; }
+        public DateTime MessageDate { get; set; }
 
     }
 }
---
+++ Villa.WebUI/ViewComponents/AdminLayout/_AdminNavHeader.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Villa.WebUI.ViewComponents.AdminLayout
+{
+    public class _AdminNavHeader : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+    }
+}
