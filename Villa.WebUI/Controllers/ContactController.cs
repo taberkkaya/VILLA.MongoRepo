4ab300562ff7d6107d8954ba7099db1e8c783ac6
taberkkaya <taberkkaya@taberkkaya.com>
2024-09-18 21:47:35 +0300

    Contact Index, create ops and dtos done

---
+++ Villa.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using Villa.Business.Abstract;
+using Villa.Dto.Dtos.ContactDtos;
+using Villa.Entity.Entities;
+
+namespace Villa.WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IContactService contactService;
+        private readonly IMapper mapper;
+
+        public ContactController(IContactService contactService, IMapper mapper)
+        {
+            this.contactService = contactService;
+            this.mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await contactService.TGetListAsync();
+            var result = mapper.Map<List<ResultContactDto>>(values);
+            return View(result);
+        }
+
+        public async Task<IActionResult> DeleteContact(ObjectId id)
+        {
+            await contactService.TDeleteAsync(id);
+            return View();
+        }
+
+        public IActionResult CreateContact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateContact(CreateContactDto createContactDto)
+        {
+            var map = mapper.Map<Contact>(createContactDto);
+            await contactService.TCreateAsync(map);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
