66684a05ba1f2d3a20a8888268d4cec8c3efbdea
taberkkaya <taberkkaya@taberkkaya.com>
2024-09-19 12:50:16 +0300

    Dtos created

--- Villa.WebUI/Mapping/GeneralMapping.cs
+++ Villa.WebUI/Mapping/GeneralMapping.cs
@@ -2,25 +2,30 @@
 using Villa.Dto.Dtos.BannerDtos;
 using Villa.Dto.Dtos.ContactDtos;
 using Villa.Dto.Dtos.CounterDtos;
+using Villa.Dto.Dtos.DealDtos;
 using Villa.Entity.Entities;
 
 namespace Villa.WebUI.Mapping
 {
-	public class GeneralMapping : Profile
-	{
-		public GeneralMapping()
-		{
-			CreateMap<ResultBannerDto, Banner>().ReverseMap();
-			CreateMap<UpdateBannerDto, Banner>().ReverseMap();
-			CreateMap<CreateBannerDto, Banner>().ReverseMap();
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<ResultBannerDto, Banner>().ReverseMap();
+            CreateMap<UpdateBannerDto, Banner>().ReverseMap();
+            CreateMap<CreateBannerDto, Banner>().ReverseMap();
 
-			CreateMap<ResultContactDto, Contact>().ReverseMap();
-			CreateMap<UpdateContactDto, Contact>().ReverseMap();
-			CreateMap<CreateContactDto, Contact>().ReverseMap();
+            CreateMap<ResultContactDto, Contact>().ReverseMap();
+            CreateMap<UpdateContactDto, Contact>().ReverseMap();
+            CreateMap<CreateContactDto, Contact>().ReverseMap();
 
-			CreateMap<ResultCounterDto, Counter>().ReverseMap();
-			CreateMap<UpdateCounterDto, Counter>().ReverseMap();
-			CreateMap<CreateCounterDto, Counter>().ReverseMap();
-		}
-	}
+            CreateMap<ResultCounterDto, Counter>().ReverseMap();
+            CreateMap<UpdateCounterDto, Counter>().ReverseMap();
+            CreateMap<CreateCounterDto, Counter>().ReverseMap();
+
+            CreateMap<ResultDealDto, Deal>().ReverseMap();
+            CreateMap<UpdateDealDto, Deal>().ReverseMap();
+            CreateMap<CreateDealDto, Deal>().ReverseMap();
+        }
+    }
 }
