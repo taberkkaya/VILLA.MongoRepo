7dd07d519e26a70f29999c75b21203510a0b4d37
akaya <taberkkaya@gmail.com>
2024-10-01 17:20:18 +0300

    VideoController and validate-rules added

---
+++ Villa.Business/Validators/VideoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Villa.Entity.Entities;
+
+namespace Villa.Business.Validators
+{
+    public class VideoValidator : AbstractValidator<Video>
+    {
+        public VideoValidator()
+        {
+            RuleFor(x => x.VideoUrl).NotEmpty();
+        }
+    }
+}
