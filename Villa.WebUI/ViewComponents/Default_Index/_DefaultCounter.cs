7bf3e87ba2bed2e8014b0c0d5b10901104f31a91
akaya <taberkkaya@gmail.com>
2024-10-02 18:20:36 +0300

    PartialViews created

---
+++ Villa.WebUI/ViewComponents/Default_Index/_DefaultCounter.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Villa.Business.Abstract;
+using Villa.Dto.Dtos.CounterDtos;
+
+namespace Villa.WebUI.ViewComponents.Default_Index
+{
+    public class _DefaultCounter : ViewComponent
+    {
+        private readonly ICounterService counterService;
+        private readonly IMapper mapper;
+
+        public _DefaultCounter(ICounterService counterService, IMapper mapper)
+        {
+            this.counterService = counterService;
+            this.mapper = mapper;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var result = await counterService.TGetListAsync();
+            var map = mapper.Map<List<ResultCounterDto>>(result);
+            return View(map);
+        }
+
+    }
+}
---
+++ Villa.WebUI/ViewComponents/Default_Index/_DefaultQuest.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Villa.Business.Abstract;
+using Villa.Dto.Dtos.QuestDtos;
+
+namespace Villa.WebUI.ViewComponents.Default_Index
+{
+    public class _DefaultQuest : ViewComponent
+    {
+        private readonly IQuestService questService;
+        private readonly IMapper mapper;
+
+        public _DefaultQuest(IQuestService questService, IMapper mapper)
+        {
+            this.questService = questService;
+            this.mapper = mapper;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var result = await questService.TGetListAsync();
+            var map = mapper.Map<List<ResultQuestDto>>(result);
+            return View(map);
+        }
+    }
+}
