6b3731d39a40f5a934000372bb3a938f1709a3eb
akaya <taberkkaya@gmail.com>
2024-10-07 16:27:55 +0300

    Contact,Deal,Message,Production done, Message ops completed

---
+++ Villa.WebUI/ViewComponents/Default_Index/_DefaultContactInfo.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Villa.Business.Abstract;
+using Villa.Dto.Dtos.ContactDtos;
+
+namespace Villa.WebUI.ViewComponents.Default_Index
+{
+    public class _DefaultContactInfo : ViewComponent
+    {
+        private readonly IContactService contactService;
+        private readonly IMapper mapper;
+
+        public _DefaultContactInfo(IContactService contactService, IMapper mapper)
+        {
+            this.contactService = contactService;
+            this.mapper = mapper;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var result = await contactService.TGetListAsync();
+            var map = mapper.Map<List<ResultContactDto>>(result);
+            return View(map);
+        }
+    }
+}
